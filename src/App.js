f5c5188699fe3b81efe706a984d797be33e5fafa
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-26 19:24:23 -0400

    setup
    moving over from old repository to see if corrupted files are fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="header-logo img-fluid" alt="logo" />
+          <h1>
+            <i>Define It!</i> App
+          </h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Hand-coded by Anne Marie Robson</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
