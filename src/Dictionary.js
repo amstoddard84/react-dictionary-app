b769105f7dae0786a302d9903ae665db2fa5043a
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-26 22:21:00 -0400

    Added search and handleKeywordChange functions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
-
+ 
 export default function Dictionary () {
+    let [keyword, setKeyword] = useState("");
+
+    function search(event) {
+        event.preventDefault();
+    alert(`Fetching the definition for ${keyword} for you!`); 
+    }
+
+    function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+    }
+
     return ( 
         <div className="Dictionary">
         <section>
 <h1 class="lookup-prompt">What word do you want to look up today?</h1>
-<form>
-    <input type="search" />
+<form onSubmit={search}>
+    <input type="search" 
+    onChange= {handleKeywordChange} />
 </form>
 <button class="btn btn-success">Define it!</button>
         </section>
