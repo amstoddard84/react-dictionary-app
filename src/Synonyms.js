62cf0885318fae1ad86c170e1e06bd8963b76041
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-29 14:10:00 -0400

    additional styling changes

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,7 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-        <section>
+        <section className="synonym-list">
         <h3 className="synonyms">Synonyms</h3>
       <ul>
         {props.synonyms.map(function (synonym, index) {
