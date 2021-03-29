156839051c15569a7efa5c7e39c7410048e880e0
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-28 20:24:55 -0400

    corrected rel=nonreferrer to noreferrer

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
     return (
         <div className="Phonetic">
-        <a className="audio-link" href={props.phonetic.audio} target="_blank" rel="nonreferrer">
+        <a className="audio-link" href={props.phonetic.audio} target="_blank" rel="noreferrer">
             <i class="fas fa-volume-up"></i> Hear it!
         </a><br></br>
         {props.phonetic.text}
