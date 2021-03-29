eab126769024741e56ac58774d4139626c7cc36f
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2021-03-28 21:07:32 -0400

    additional styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
         <section>
-        <h3>Synonyms</h3>
+        <h3 className="synonyms">Synonyms</h3>
       <ul>
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
